5f814eb581581f5614e27f0da609fc8612af7162
Matthias Claassen <60026642+mwclaass@users.noreply.github.com>
2022-03-08 17:11:43 -0500

    Imported Input System and GitHub for Unity packages. Also, created GameManager and EventManager classes.

---
+++ Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager instance;
+
+    // To declare a UnityEvent:
+    // public static UnityEvent NewEvent1 = new UnityEvent();
+    // public static UnityEvent<int> NewEvent2 = new UnityEvent<int>();
+
+    // To invoke a UnityEvent (from another class):
+    // EventManager.NewEvent1?.Invoke();
+    // EventManager.NewEvent2?.Invoke(0);
+
+    // To subscribe to a UnityEvent:
+    // EventManager.NewEvent1.AddListener(NameOfFunctionWithoutParameters);
+    // EventManager.NewEvent2.AddListener(NameOfFunctionWithIntParameter);
+
+    // All events should be grouped under an appropriate heading, such as one of the 
+    // following:
+
+    // GAME STATE EVENTS
+
+    // PLAYER EVENTS
+
+    // etc.
+
+    private void Awake()
+    {
+        // Not checking to see if another instance exists because if we switch scenes
+        // then we'll want our EventManager instance to become the one for the current
+        // scene
+        instance = this;
+    }
+}
