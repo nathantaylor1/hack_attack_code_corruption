c343b70b2868040daf45d75a6a27ca07fc04d6f0
jbusa22 <jbusa@umich.edu>
2022-03-12 12:37:29 -0500

    Working tabs

---
+++ Assets/CodeEditorSwapper.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CodeEditorSwapper : MonoBehaviour
+{
+    public Button currentButton;
+    public GameObject currentCanvas;
+    public Color selectedColor;
+    public GameObject buttons;
+    public GameObject canvases;
+    public Dictionary<int, GameObject> buttonIdToCanvas = new Dictionary<int, GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (buttons.transform.childCount != canvases.transform.childCount) {
+            Debug.LogError("Buttons must map to Canvases");
+        }
+        
+        // select the current button on first load
+        var firstTime = currentButton;
+        currentButton = null;
+        firstTime.Select();
+
+        for (int i = 0; i < buttons.transform.childCount; i++)
+        {
+            // Change selected color option for all buttons instead of in editor
+            var tempButton = buttons.transform.GetChild(i).GetComponent<Button>();
+            var newColors = tempButton.colors;
+            newColors.selectedColor = selectedColor;
+            tempButton.colors = newColors;
+
+            buttonIdToCanvas.Add(buttons.transform.GetChild(i).gameObject.GetInstanceID(), canvases.transform.GetChild(i).gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void swap(Button buttonPressed) 
+    {
+        if (currentButton != buttonPressed) {
+            var buttonColors = buttonPressed.colors;
+            // old button resets
+            if (currentButton) {
+                currentButton.colors = buttonColors;
+            }
+            buttonColors.normalColor = selectedColor;
+            buttonColors.highlightedColor = selectedColor;
+            // new button changes
+            buttonPressed.colors = buttonColors;
+            currentButton = buttonPressed;
+
+            var canvas = buttonIdToCanvas[buttonPressed.gameObject.GetInstanceID()];
+            // old canvas moves out of view
+            currentCanvas.SetActive(false);
+            // new canvas comes into view
+            canvas.SetActive(true);
+            currentCanvas = canvas;
+        }
+    }
+}
