77a43f83ddbac2b984942f9c0f208d8935fc1437
rsorkin1106 <55860014+rsorkin1106@users.noreply.github.com>
2022-03-22 20:09:50 -0400

    Updated initial bounds of boxcast

--- Assets/Scripts/Utilities/Grounded.cs
+++ Assets/Scripts/Utilities/Grounded.cs
@@ -18,8 +18,11 @@ public class Grounded
     public static bool Check(Collider2D col)
     {
         Bounds bounds = col.bounds;
-        Vector3 extents = bounds.extents * 2 + new Vector3(-0.1f, 0, 0);
-        if (Physics2D.BoxCast(bounds.center, extents, 0f, Vector2.down, 0.05f, ~(1 << col.gameObject.layer)))
+        Vector3 lowered_bounds = bounds.center +  new Vector3(0, -.2f,0);
+        //lowered_bounds.y /= 2;
+        Vector3 extents = bounds.extents + new Vector3(-0.1f, 0, 0);
+        
+        if (Physics2D.BoxCast(lowered_bounds, extents, 0f, Vector2.down, 0.05f, ~(1 << col.gameObject.layer)))
         {
             return true;
         }
