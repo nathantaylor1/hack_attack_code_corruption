eea2f7a764aaed27169608a6e0ac4e019cb0d567
Matthias Claassen <60026642+mwclaass@users.noreply.github.com>
2022-03-25 02:15:12 -0400

    Added new printer animations

--- Assets/Scripts/Utilities/Grounded.cs
+++ Assets/Scripts/Utilities/Grounded.cs
@@ -18,9 +18,9 @@ public class Grounded
     public static bool Check(Collider2D col)
     {
         Bounds bounds = col.bounds;
-        Vector3 extents = bounds.extents * 2 + new Vector3(-0.1f, 0, 0);
+        Vector3 extents = bounds.extents * 2 + new Vector3(-0.1f, -0.2f, 0);
         
-        if (Physics2D.BoxCast(bounds.center, extents, 0f, Vector2.down, 0.1f, ~(1 << col.gameObject.layer)))
+        if (Physics2D.BoxCast(bounds.center + Vector3.down * 0.1f, extents, 0f, Vector2.down, 0.05f, ~(1 << col.gameObject.layer)))
         {
             return true;
         }
