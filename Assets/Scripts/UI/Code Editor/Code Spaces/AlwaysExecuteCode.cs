d891ec5618f088b1d299d3060ea1dcc5dc220f11
rsorkin1106 <55860014+rsorkin1106@users.noreply.github.com>
2022-03-26 15:15:51 -0400

    Added an AlwaysExecute Codespace

---
+++ Assets/Scripts/UI/Code Editor/Code Spaces/AlwaysExecuteCode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysExecuteCode : CodeSpace
+{
+    private void Update()
+    {
+        StartExecution();
+    }
+}
