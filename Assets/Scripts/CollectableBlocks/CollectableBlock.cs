c795ec08b3714c6c09529c99c90a0b1c8beb438e
Matthias Claassen <60026642+mwclaass@users.noreply.github.com>
2022-03-28 01:25:32 -0400

    finished rough draft of custom level

--- Assets/Scripts/CollectableBlocks/CollectableBlock.cs
+++ Assets/Scripts/CollectableBlocks/CollectableBlock.cs
@@ -6,7 +6,8 @@ public class CollectableBlock : MonoBehaviour
 {
     public GameObject block;
     private bool alreadyPickedUp = false;
-    public float quick = 1.6f;
+    public float slow = 1.6f;
+    public float quick = 2f;
     public AudioClip pickupSound;
 
     private void OnTriggerEnter2D(Collider2D other) {
@@ -23,6 +24,10 @@ public class CollectableBlock : MonoBehaviour
     {
         if (alreadyPickedUp) return;
         alreadyPickedUp = true;
+        //Transform codeBlock = Instantiate(gameObject.transform.GetChild(0).GetChild(0));
+        GameObject codeBlock = Instantiate(block);
+        InventoryManager.instance.AddBlock(codeBlock.GetComponent<Code>());
+        //codeBlock.transform.localScale = new Vector3(1, 1, 1);
         StartCoroutine(moveToward());
 
         if (pickupSound != null && AudioManager.instance != null && Camera.main != null)
@@ -32,29 +37,35 @@ public class CollectableBlock : MonoBehaviour
     private IEnumerator moveToward() {
         var close = false;
         var screenPos = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 1.5f + Vector3.right * 1);
+        float lastTime = Time.time;
         while (!close) {
+            float deltaTime = Time.time - lastTime;
             var temp = Camera.main.ScreenToWorldPoint(screenPos);
             temp.z = 0;
-            transform.position = Vector3.Lerp(transform.position, temp, Time.fixedDeltaTime * 1.2f);
+            transform.position = Vector3.Lerp(transform.position, temp, deltaTime * slow);
             if (Vector3.Distance(temp, transform.position) < 1.5f) {
                 close = true;
             }
+            lastTime = Time.time;
             yield return null;
         }
         close = false;
-        while (!close) {
-            var temp = Camera.main.ScreenToWorldPoint(block.transform.position);
+        while (!close)
+        {
+            float deltaTime = Time.time - lastTime;
+            var temp = Camera.main.ScreenToWorldPoint(InventoryImage.instance.transform.position);
             temp.z = 0;
-            transform.position = Vector3.Lerp(transform.position, temp, Time.fixedDeltaTime * quick);
+            transform.position = Vector3.Lerp(transform.position, temp, deltaTime * quick);
             if (Vector3.Distance(temp, transform.position) < 1.7f) {
                 close = true;
             }
+            lastTime = Time.time;
             yield return null;
         }
         // Pickup blocks must be nested exactly two levels
-        Transform codeBlock = gameObject.transform.GetChild(0).GetChild(0);
+        /*Transform codeBlock = gameObject.transform.GetChild(0).GetChild(0);
         InventoryManager.instance.AddBlock(codeBlock.GetComponent<Code>());
-        codeBlock.localScale = new Vector3(1, 1, 1);
+        codeBlock.localScale = new Vector3(1, 1, 1);*/
         Destroy(gameObject);
     }
 }
