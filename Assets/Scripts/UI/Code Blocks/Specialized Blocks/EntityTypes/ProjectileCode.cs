ba79126856261bc4cf54e0e69e5d31b6895ef45f
jbusa22 <jbusa@umich.edu>
2022-04-01 19:00:42 -0400

    projectile code block

---
+++ Assets/Scripts/UI/Code Blocks/Specialized Blocks/EntityTypes/ProjectileCode.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class ProjectileCode : Code
+{
+    public GameObject prefab;
+    [SerializeField] 
+    protected Image entityImage;
+    protected override void Awake() {
+        base.Awake();
+        entityImage.sprite = prefab.GetComponent<SpriteRenderer>().sprite;
+    }
+    public override dynamic ReturnValue()
+    {
+        return prefab;
+    }
+}
