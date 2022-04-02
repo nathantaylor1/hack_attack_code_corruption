4c6b61d0128def546ceff6e092aa576a57566605
Nathan Taylor <nntaylor@umich.edu>
2022-04-02 19:05:58 -0400

    Rotation is buggy

--- Assets/Scripts/UI/Code Blocks/Specialized Blocks/Movement/CrawlCode.cs
+++ Assets/Scripts/UI/Code Blocks/Specialized Blocks/Movement/CrawlCode.cs
@@ -10,7 +10,9 @@ public class CrawlCode : CodeWithParameters
     private Collider2D col;
     private Transform tf;
     private Animator anim;
+    
     private float right;
+    private bool facingRight = true;
 
     public override void ExecuteCode()
     {
@@ -19,14 +21,25 @@ public class CrawlCode : CodeWithParameters
         tf = module.transform;
         anim = module.anim;
         
+        Rotate();
         GetDirection();
 
-        Vector2 mvmt = tf.right * right;
+        if (!facingRight && right > 0)
+        {
+            Debug.Log("MOVE RIGIHT");
+            facingRight = true;
+            tf.Rotate(tf.up, 180f);
+        }
+        else if (facingRight && right < 0)
+        {
+            Debug.Log("MOVE LEFT ");
+            facingRight = false;
+            tf.Rotate(tf.up, 180f);
+        }
+
+        float speed = module.moveSpeed * (float)(object)GetParameter(1);
+        Vector2 mvmt = tf.right * speed + (-tf.up * 2f);
         rb.velocity = mvmt;
-        if (mvmt == Vector2.zero)
-            if (anim != null) anim.SetTrigger("Idle");
-        else
-            if (anim != null) anim.SetTrigger("Run");
 
         base.ExecuteCode();
     }
@@ -34,38 +47,78 @@ public class CrawlCode : CodeWithParameters
     private void GetDirection()
     {
         Vector2 dir = ((Vector2)(object)GetParameter(0)).normalized;
-        float speed = module.moveSpeed * (float)(object)GetParameter(1);
         float angle = tf.localEulerAngles.z;
         switch (Mathf.Abs(angle))
         {
             case 0: case 360:
-                right = dir.x * speed;
+                right = dir.x;
                 break;
             
             case 180:
-                right = -dir.x * speed;
+                right = -dir.x;
                 break;
             
             case 90:
-                right = dir.y * speed;
+                right = dir.y;
                 break;
             
             case 270:
-                right = -dir.y * speed;
+                right = -dir.y;
                 break;
 
             default:
                 // Assert is in line with a surface (must be multiple of 90 degrees)
                 float newAngle = Mathf.Round(angle / 90) * 90;
-                tf.eulerAngles = new Vector3(0f, 0f, newAngle);
+                tf.eulerAngles = new Vector3(tf.eulerAngles.x, tf.eulerAngles.y, newAngle);
                 break;
         }
     }
 
     public override void StopExecution()
     {
-        rb.velocity = Vector2.zero;
+        rb.velocity = new Vector2(0f, rb.velocity.y);
         if (anim != null) anim.SetTrigger("Idle");
         base.StopExecution();
     }
+
+    private bool canTurn, cooldown;
+    private void Rotate()
+    {
+        if (cooldown) return;
+        Vector3 origin = col.bounds.center;
+        LayerMask lm = 1 << LayerMask.NameToLayer("Ground");
+        if (!Physics2D.Raycast(origin, -tf.up, 0.5f, lm))
+        {
+            if (canTurn)
+            {
+                cooldown = true;
+                Vector3 lea = tf.localEulerAngles;
+                lea.z += 90f;
+                tf.localEulerAngles = lea;
+                canTurn = false;
+            }
+        }
+        else
+        {
+            // Wait to be able to turn until after back on the ground
+            canTurn = true;
+        }
+        if (Physics2D.Raycast(origin, tf.right, 1.0f, lm))
+        {
+            cooldown = true;
+            Vector3 lea = tf.localEulerAngles;
+            lea.z -= 90f;
+            tf.localEulerAngles = lea;
+        }
+
+        if (cooldown) StartCoroutine(CO_Cooldown());
+    }
+
+    private IEnumerator CO_Cooldown()
+    {
+        yield return new WaitForSeconds(0.25f);
+        cooldown = false;
+        yield return null;
+    }
+
 }
