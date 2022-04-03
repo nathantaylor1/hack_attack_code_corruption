c393bc67463ce6f532503288b0bf1ca0924caff5
Nathan Taylor <nntaylor@umich.edu>
2022-04-03 07:36:28 -0400

    Crawler implemented with and without blocks (Can't test yet)
    Cannot test the crawler in the block code because idk how to add blocks to an enemy

--- Assets/Scripts/Enemies/CrawlNoBlock.cs
+++ Assets/Scripts/Enemies/CrawlNoBlock.cs
@@ -1,47 +1,46 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-public class CrawlCode : CodeWithParameters
+public class CrawlNoBlock : MonoBehaviour
 {
+    public float speed = 2.0f;
     private Rigidbody2D rb;
     private Collider2D col;
     private Bounds bds;
-    private Transform tf;
-    private Animator anim;
-
+    private readonly LayerMask glm = 1 << 7 /*LayerMask.NameToLayer("Ground") is 7*/;
     private bool hasTurned;
-    private readonly LayerMask glm = 1 << 7;
 
-    public override void ExecuteCode()
+    private void Awake()
     {
-        rb = module.rb;
-        col = module.col;
-        bds = col.bounds;
-        tf = module.transform;
-        anim = module.anim;
-
-        CheckWall();
-        CheckGround();
-
-        Move();
-
-        base.ExecuteCode();
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+        //Debug.Log("LayerMask: " + LayerMask.NameToLayer("Ground"));
     }
 
-    public override void StopExecution()
+    private void Update()
     {
-        rb.velocity = new Vector2(0f, rb.velocity.y);
-        base.StopExecution();
+        CheckWall();
+        CheckGround();
+    }
+    
+    private void FixedUpdate()
+    {
+        bds = col.bounds;
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = transform.right * speed;
     }
 
     void CheckGround()
     {
         Vector2 origin = bds.center;
         Vector2 size = new Vector2(2f * bds.extents.x - 0.05f, 2 * bds.extents.y - 0.05f);
-        Vector2 direction = -tf.up;
+        Vector2 direction = -transform.up;
         
         RaycastHit2D hit = Physics2D.BoxCast(origin, size, 0f, direction, 0.05f, glm);;
 
@@ -54,26 +53,19 @@ public class CrawlCode : CodeWithParameters
         if (!hasTurned) return;
 
         hasTurned = false;
-        tf.Rotate(tf.forward, -90f);
+        transform.Rotate(transform.forward, -90f);
     }
 
     void CheckWall()
     {
         Vector2 origin = bds.center;
         Vector2 size = new Vector2(2f * bds.extents.x - 0.05f, 2 * bds.extents.y - 0.05f);
-        Vector2 direction = tf.right;
+        Vector2 direction = transform.right;
         
         RaycastHit2D hit = Physics2D.BoxCast(origin, size, 0f, direction, 0.05f, glm);;
         
         if (!hit) return;
         
-        tf.Rotate(tf.forward, 90f);
-    }
-    
-    private void Move()
-    {
-        Vector2 dir = (Vector2)(object)GetParameter(0); // TODO
-        float speed = module.moveSpeed * (float)(object)GetParameter(1);
-        rb.velocity = transform.right * speed;
+        transform.Rotate(transform.forward, 90f);
     }
 }
