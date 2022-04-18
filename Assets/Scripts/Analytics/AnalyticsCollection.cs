541f7aa9971d9882b2727a5e484e8f5060054859
Nathan Taylor <nntaylor@umich.edu>
2022-04-18 11:15:40 -0400

    Analytics Main Menu hotfix
    made it so analytics question only needs to be answered one time

--- Assets/Scripts/Analytics/AnalyticsCollection.cs
+++ Assets/Scripts/Analytics/AnalyticsCollection.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Analytics;
 // https://docs.unity3d.com/ScriptReference/Analytics.Analytics-enabled.html
 
-public class AnalyticsCollection : MonoBehaviour 
+public class AnalyticsCollection : MonoBehaviour
 {
+    public GameObject window;
+    public GameObject tab;
+    
     public static void LevelComplete(int levelNum)
     {
         if (Application.isEditor) return;
@@ -40,14 +44,27 @@ public class AnalyticsCollection : MonoBehaviour
     public static void DisableAnalyticsCompletely()
     {
         Analytics.enabled = false;
+        AnalyticsStorage.enabled = false;
         Analytics.deviceStatsEnabled = false;
         PerformanceReporting.enabled = false;
+        AnalyticsStorage.hasAnswered = true;
     }
 
     public static void ReEnableAnalytics()
     {
         Analytics.enabled = true;
+        AnalyticsStorage.enabled = true;
         Analytics.deviceStatsEnabled = true;
         PerformanceReporting.enabled = true;
+        AnalyticsStorage.hasAnswered = true;
+    }
+
+    private void Start()
+    {
+        if (!AnalyticsStorage.hasAnswered)
+        {
+            window.SetActive(true);
+            tab.SetActive(true);
+        }
     }
 }
